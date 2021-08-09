c6541713181b46d04dcdd3c55ddfe3919fd2ad1f
Manan <gouhari21@hotmail.com>
2021-08-10 00:23:04 +0530

    feat: fetch top 3 earning VGs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fetchMarketCapAndPrice = require("./lib/data/celo-market-details");
+const fetchTopEarningVGs = require("./lib/data/top-earning-vg");
 
 (async function main() {
-  console.log(await fetchMarketCapAndPrice());
+  // console.log(await fetchMarketCapAndPrice());
+  console.log(await fetchTopEarningVGs());
 })();
