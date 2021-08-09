c71404ca14b2991ce6ee5b06c9c0d3fead47407a
Manan <gouhari21@hotmail.com>
2021-08-10 03:27:28 +0530

    feat: find top 3 CHs. Find all rewards earned by CH through ChurroFi.:

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const fetchMarketCapAndPrice = require("./lib/data/celo-market-details");
-const fetchTopEarningVGs = require("./lib/data/top-earning-vg");
-
 (async function main() {
-  // console.log(await fetchMarketCapAndPrice());
-  console.log(await fetchTopEarningVGs());
+  console.log("Main logic goes here.");
 })();
